6257d466225d3f4923fa6bfc2d9b53f9a812e820
Wenlin Huang <jackhwl@hotmail.com>
2020-03-20 09:28:16 -0400

    pint-free

---
+++ point-free/ex.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// function output(txt) {
+// 	console.log(txt);
+// }
+
+var output = console.log.bind(console);
+//output("tt")
+
+function when(fn) {
+    return function(predicate){
+        return function(...args){
+            if (predicate(...args)) {
+                return fn(...args);
+            }
+        };
+    };
+}
+
+// function printIf(shouldPrintIt) {
+// 	return function(msg) {
+// 		if (shouldPrintIt(msg)) {
+// 			output(msg);
+// 		}
+// 	};
+// }
+var printIf = when(output);
+
+function not(fn) {
+    return function negated(...args){
+        return !fn(...args)
+    }
+}
+
+function isShortEnough(str) {
+	return str.length <= 5;
+}
+
+// function isLongEnough(str) {
+// 	return !isShortEnough(str);
+// }
+
+var isLongEnough = not(isShortEnough)
+
+var msg1 = "Hello";
+var msg2 = msg1 + " World";
+
+printIf(isShortEnough)(msg1);		// Hello
+printIf(isShortEnough)(msg2);
+printIf(isLongEnough)(msg1);
+printIf(isLongEnough)(msg2);		// Hello World
