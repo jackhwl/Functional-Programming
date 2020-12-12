0a6d58709ce8b8d717ee2c2784bd038f7a4b9fb0
Wenlin Huang <jackhwl@hotmail.com>
2020-12-12 10:45:51 -0500

    home made filter map

--- functional-first-steps/high-order.js
+++ functional-first-steps/high-order.js
@@ -19,3 +19,29 @@ function filter(predicateFn, array) {
     (n) => { return n > 4; }, // TODO replace this line
     wholes
   )
+
+  function isPrime(n) {
+    // TODO your code goes here
+    if (n<2) return false;
+    var counter = 0;
+    for(var i = 2; i<=Math.sqrt(n); i++){
+      counter += (n % i) === 0 ? 1 : 0;
+    }
+    return counter===0;
+  }
+
+  function map(fn, array) {
+    // TODO your code goes here
+    if (length(array)===0) return [];
+    const firstItem = head(array);
+    const firstEle = [fn(firstItem)];
+    return concat(firstEle, map(fn, tail(array)));
+  }
+
+  fizzBuzz = map(n => {
+    // TODO your code goes here
+    if (n % 15 === 0) return 'fizzbuzz';
+    if (n % 3 === 0) return 'fizz';
+    if (n % 5 === 0) return 'buzz';
+    return n;
+  }, wholes)
